23f0b626f5f7ea53da52ac82bbee2247ebb462a3
Ashwaryapratap_Singh <ashwarya.singh@ext.zestmoney.in>
2023-07-17 12:52:41 +0530

    CQRS_Implementation

---
+++ ProductService/src/main/java/com/incubation/ash/ProductService/query/api/queries/GetProductsQuery.java
@@ -0,0 +1,4 @@
+package com.incubation.ash.ProductService.query.api.queries;
+
+public class GetProductsQuery {
+}
---
+++ ProductService/src/test/java/com/incubation/ash/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,9 @@
+package com.incubation.ash.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+}
